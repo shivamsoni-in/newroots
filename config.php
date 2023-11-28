7b59e953cb63ab76eef85aba09dc47a75442b126
shivamsoni-in <152253499+shivamsoni-in@users.noreply.github.com>
2023-11-28 14:39:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,1 @@
-<?php
-
-	// Database configuration 
-
-	$sernamename = "localhost";
-	$username    = "iiijssmy_newroot";
-	$passoword   = "newrootclinic";
-	$databasename= "iiijssmy_newroot";
-
-	// Create database connection
-	$con = mysqli_connect($sernamename, $username,$passoword,$databasename);
-
-	// Check connection
-	if ($con->connect_error) {
-		die("Connection failed". $con->connect_error);
-	}
-
-?>
+404
